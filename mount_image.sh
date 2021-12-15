75c5e4b3f90bae809a7237a012b57eef1773061c
Mike Haller <mike.haller@bosch-si.com>
2021-12-15 12:35:58 +0100

    Experiment

--- mount_image.sh
+++ mount_image.sh
@@ -11,8 +11,8 @@ echo "Image: ${image}"
 echo "Additional MB: ${additional_mb}"
 
 if [[ "" -eq "${image}" ]]; then
-  echo "Error: no image"
-  exit 0
+  echo "Error: no image, fallback to image=debian-11.1.0-arm64-netinst.iso"
+  image="image=debian-11.1.0-arm64-netinst.iso"
 fi
 
 if [ ${additional_mb} -gt 0 ]; then
